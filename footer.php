5853bed9f4bc9a963da1efe9ac5d46ca803aff04
Nivedika-2001 <92134709+Nivedika-2001@users.noreply.github.com>
2021-11-17 15:28:00 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,4 @@
+<div id="container">
+    <div id="header">Made by Nivedika Selika <br>
+      Project of The Sparks Foundation</div>
+ </div>
---
+++ sparks_bank.sql
@@ -0,0 +1,106 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sparks_bank`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaction`
+--
+
+CREATE TABLE `transaction` (
+  `sno` int(3) NOT NULL,
+  `sender` text NOT NULL,
+  `receiver` text NOT NULL,
+  `balance` int(8) NOT NULL,
+  `datetime` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `transaction`
+--
+
+INSERT INTO `transaction` (`sno`, `sender`, `receiver`, `balance`, `datetime`) VALUES
+(2, 'Raju', 'Deepthi', 800, '2021-04-08 18:08:56'),
+(3, 'Nivedika', 'Jasmin', 1000, '2021-04-08 14:55:43'),
+(4, 'Raju', 'Tejas', 10, '2021-04-08 14:58:55'),
+(5, 'Aman', 'Krishna', 4000, '2021-06-03 21:49:16'),
+(6, 'Krishna', 'Aman', 5000, '2021-06-05 20:41:06'),
+(7, 'Aman', 'Krishna', 3000, '2021-06-05 23:34:40'),
+(8, 'Krishna', 'Aman', 2500, '2021-06-06 10:42:24');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(3) NOT NULL,
+  `name` text NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `balance` int(8) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `balance`) VALUES
+(1, 'Aman', 'amankhan@gmail.com', 50890),
+(2, 'Krishna', 'krishnapatil@gmail.com', 30900),
+(3, 'Nivedika', 'nivedikashah@gmail.com', 49900),
+(4, 'Priyanka', 'priyanka10@gmail.com', 10000),
+(5, 'Shubham', 'shubham1489@gmail.com', 40060),
+(6, 'Raju', 'rajusingh@gmail.com', 19770),
+(7, 'Deepthi', 'deepthi1298@gmail.com', 50000),
+(8, 'Jasmin', 'jasminjain29@gmail.com', 30000),
+(9, 'Naman', 'dixitnaman68@gmail.com', 69000),
+(10, 'Tejas', 'tejas34@gmail.com', 35009);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `transaction`
+--
+ALTER TABLE `transaction`
+  ADD PRIMARY KEY (`sno`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `transaction`
+--
+ALTER TABLE `transaction`
+  MODIFY `sno` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
